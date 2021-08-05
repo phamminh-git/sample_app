45015a2767540970c7c6f9f528a08c897a2e5367
PhamMinh <phamminh@ubuntu.ubuntu-domain>
2021-08-05 15:15:28 +0700

    Chapter 11: Account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  PARAMS = %i(name email password password_confirmation).freeze
   before_save :downcase_email
+  before_create :create_activation_digest
+  attr_accessor :remember_token, :activation_token
 
   validates :name, presence: true, length: {
     minimum: Settings.name.length.minimum,
@@ -13,11 +16,9 @@ class User < ApplicationRecord
   validates :password, presence: true, length: {
     minimum: Settings.password.length.minimum,
     maximum: Settings.password.length.maximum
-  }
+  }, allow_nil: true, if: :password
 
   has_secure_password
-
-  attr_accessor :remember_token
 
   class << self
     def digest string
@@ -36,18 +37,32 @@ class User < ApplicationRecord
     update remember_digest: User.digest(remember_token)
   end
 
-  def authenticated? remember_token
-    return false unless remember_token
+  def authenticated? attribute, token
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
 
-    BCrypt::Password.new(remember_digest).is_password? remember_token
+    BCrypt::Password.new(digest).is_password? token
   end
 
   def forget
     update remember_digest: nil
   end
 
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  def activate
+    update_columns activated: true, activated_at: Time.zone.now
+  end
+
   private
   def downcase_email
     email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest activation_token
   end
 end
